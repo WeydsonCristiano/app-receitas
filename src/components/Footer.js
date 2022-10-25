d0152dffc297ce29420099dac58c87d1f972a849
alysson-tobias <alyssonfontes8@gmail.com>
2022-10-25 18:13:19 -0300

    erros linte-css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
-import './Footer.css';
 
 function Footer() {
   return (
