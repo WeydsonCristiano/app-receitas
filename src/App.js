6e87e1c1f29fc7f847478e97a41d98c664ef103d
IsraelPereira <pereira.israel2070@gmail.com>
2022-10-27 10:19:07 -0300

    refatora a recipes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
 import Recipes from './pages/Recipes';
@@ -13,28 +13,34 @@ import RecipeProvider from './context/recipeProvider';
 
 function App() {
   return (
-    <BrowserRouter>
-      <RecipeProvider>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route path="/meals" component={ Recipes } />
-          <Route path="/drinks" component={ Recipes } />
-          <Route path="/meals/:id-da-receita" component={ RecipeDetails } />
-          <Route path="/drinks/:id-da-receita" component={ RecipeDetails } />
-          <Route
-            path="/meals/:id-da-receita/in-progress"
-            component={ RecipeInProgress }
-          />
-          <Route
-            path="/drinks/:id-da-receita/in-progress"
-            component={ RecipeInProgress }
-          />
-          <Route path="/done-recipes" component={ DoneRecipes } />
-          <Route path="/favorite-recipes" component={ FavoriteRecipes } />
-          <Route path="/profile" component={ Profile } />
-        </Switch>
-      </RecipeProvider>
-    </BrowserRouter>
+    <RecipeProvider>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/meals" component={ Recipes } />
+        <Route exact path="/drinks" component={ Recipes } />
+        <Route
+          exact
+          path="/meals/:id"
+          render={ (props) => <RecipeDetails { ...props } /> }
+        />
+        <Route
+          exact
+          path="/drinks/:id"
+          render={ (props) => <RecipeDetails { ...props } /> }
+        />
+        <Route
+          path="/meals/:id-da-receita/in-progress"
+          component={ RecipeInProgress }
+        />
+        <Route
+          path="/drinks/:id-da-receita/in-progress"
+          component={ RecipeInProgress }
+        />
+        <Route path="/done-recipes" component={ DoneRecipes } />
+        <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+        <Route path="/profile" component={ Profile } />
+      </Switch>
+    </RecipeProvider>
   );
 }
 
--- src/components/CardRecipes.js
+++ src/components/CardRecipes.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
+import { useContext } from 'react';
+import recipeContext from '../context/recipeContext';
 
-export default function CardRecipes(index, name, thumb) {
+export default function CardRecipes(index, name, thumb, id) {
+  const { history } = useContext(recipeContext);
+  const { location: { pathname } } = history;
   return (
-    <Card key={ index } data-testid={ `${index}-recipe-card` }>
-      <Card.Img
-        variant="top"
-        data-testid={ `${index}-card-img` }
-        src={ thumb }
-        alt={ name }
-      />
-      <Card.Footer data-testid={ `${index}-card-name` }>{name}</Card.Footer>
-    </Card>
+    <Link
+      to={
+        pathname === '/meals' ? `/meals/${id}` : `/drinks/${id}`
+      }
+      key={ index }
+      data-testid={ `${index}-recipe-card` }
+    >
+      <Card>
+        <Card.Img
+          variant="top"
+          data-testid={ `${index}-card-img` }
+          src={ thumb }
+          alt={ name }
+        />
+        <Card.Footer data-testid={ `${index}-card-name` }>{name}</Card.Footer>
+      </Card>
+    </Link>
   );
 }
