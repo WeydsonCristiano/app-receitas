e8db0a019c4c8c3279e1f4b768ce8c62a62d11dd
alysson-tobias <alyssonfontes8@gmail.com>
2022-10-28 11:48:47 -0300

    correcao erros

---
+++ src/components/FavoriteRecipes.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import Container from 'react-bootstrap/Container';
+// import drinkIcon from '../images/drinkIcon.svg';
+// import mealIcon from '../images/mealIcon.svg';
+// import './styles/footer.css';
+
+// function FavoritesRecipes() {
+
+//   const handlefavotires = (e) => {
+//     // e.preventDefault();
+//     // const user = { email, password };
+//     // setUserInfo(user);
+//     // localStorage.setItem('user', JSON.stringify({ email }));
+//     // history.push('/meals');
+//   };
+
+//   return (
+
+//   );
+// }
+
+// export default FavoritesRecipes;
